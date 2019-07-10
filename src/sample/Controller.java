56071a32ef141ae9499ed25a98b5f4ac7fb7e67c
ADNAN <adnan.ali.yari@gmail.com>
2019-07-10 18:02:56 +0430

    plotter-V2.0-

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,13 +15,13 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import com.fazecast.jSerialComm.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXML;
 
 
@@ -39,6 +39,7 @@ public class Controller {
     private int setupCount = 0;
     private SerialPort serialPort;
     private SimpleIntegerProperty SIZE_OF_CHART = new SimpleIntegerProperty(20);
+    private SimpleDoubleProperty STEP_OF_CHART = new SimpleDoubleProperty(0.5);
     private SimpleBooleanProperty loopCondition = new SimpleBooleanProperty(true);
 
     /**
@@ -49,9 +50,9 @@ public class Controller {
      *
      */
     @FXML
-    JFXTextField sizeOfChartTextArea;
+    JFXTextField sizeOfChartTextArea, x_axisStep;
     @FXML
-    JFXComboBox<SerialPort> portNames;
+    JFXComboBox<String> portNames;
     @FXML
     JFXComboBox<Integer> baudRate;
     @FXML
@@ -102,6 +103,7 @@ public class Controller {
         baudRate.getItems().add(115200);
         baudRate.getItems().add(128000);
         baudRate.getItems().add(256000);
+        baudRate.setValue(110);
     }
     
     private void dataBitsSetup(){
@@ -109,12 +111,13 @@ public class Controller {
         dataBits.getItems().add(6);
         dataBits.getItems().add(7);
         dataBits.getItems().add(8);
+        dataBits.setValue(5);
     }
 
     private void stopBitSetup(){
         stopBit.getItems().add(1);
-        stopBit.getItems().add(1_5);
         stopBit.getItems().add(2);
+        stopBit.setValue(1);
     }
 
     private void paritySetup(){
@@ -123,22 +126,22 @@ public class Controller {
         parity.getItems().add("Mark");
         parity.getItems().add("Space");
         parity.getItems().add("None");
+        parity.setValue("None");
     }
-/*
+
     private int paritySetter(){
         if (parity.getValue().equalsIgnoreCase("EVEN"))
-            return ;
+            return 2;
         if (parity.getValue().equalsIgnoreCase("ODD"))
-            return ;
+            return 1;
         if (parity.getValue().equalsIgnoreCase("MARK"))
-            return SerialPort.PARITY_MARK;
+            return 3;
         if (parity.getValue().equalsIgnoreCase("SPACE"))
-            return SerialPort.PARITY_SPACE;
+            return 4;
         if (parity.getValue().equalsIgnoreCase("NONE"))
-            return SerialPort.PARITY_NONE;
+            return 0;
         else return 0;
     }
-    */
     
     /**
      *
@@ -152,10 +155,11 @@ public class Controller {
      * before commit and push in git
      *
      */
-
+    
     double x = 0;
-
+    double y;
     XYChart.Series<String, Integer> series;
+
     @FXML
     private void setUp(){
         if (setupCount == 0){
@@ -168,6 +172,10 @@ public class Controller {
             baudRateSetup();
             dataBitsSetup();
             stopBitSetup();
+            paritySetup();
+            /**
+             * this thread is a simple meaningless exp
+             */
             /*
             thread = new Thread(()->{
                 while (loopCondition.get()){
@@ -196,9 +204,23 @@ public class Controller {
                 }
             });
             thread.start();
-            paritySetup();
             */
             ++setupCount;
+        }
+    }
+
+    private void textAreaChanger(int x, int y){
+        dialogPane.appendText("\n["+LOGS.INFO+"]\t\t\t\t\t "
+                +"x : "+x
+                +"\t\ty : "+y);
+    }
+
+    @FXML
+    private void chartChanger(){
+        if (!sizeOfChartTextArea.getText().isEmpty()
+            && !x_axisStep.getText().isEmpty()) {
+            SIZE_OF_CHART.set(new Integer(sizeOfChartTextArea.getText()));
+            STEP_OF_CHART.set(new Double(x_axisStep.getText()));
         }
     }
 
@@ -208,74 +230,62 @@ public class Controller {
      * and refresh port ChoiceBox
      *
      */
-
-
-    private void textAreaChanger(int x, int y){
-        dialogPane.appendText("\n["+LOGS.INFO+"]\t\t\t\t\t "
-                +"x : "+x
-                +"\t\ty : "+y);
-    }
-
-    @FXML
-    private void EOO(){
-        if (thread.isAlive()){
-            System.out.println("thread is stopped");
-            thread.stop();
-        }
-        else{
-            System.out.println("thread is started");
-            thread.resume();
-        }
-    }
-
-    @FXML
-    private void chartChanger(){
-        SIZE_OF_CHART.set(new Integer(sizeOfChartTextArea.getText()));
-    }
-
     @FXML
     private void refresh(){
         SerialPort[] list = SerialPort.getCommPorts();
         portNames = new JFXComboBox<>();
         for (int i = 0; i < list.length; i++){
-            portNames.getItems().add(list[i]);
+            portNames.getItems().add(list[i]
+                    .getDescriptivePortName());
         }
 
     }
 
     @FXML
     private void setButton(){
-        System.out.println("set button clicked!");
-        serialPort = portNames.getValue();
+        /**
+         * setting parameters of serial port
+         */
+        serialPort = SerialPort.getCommPort(portNames.getValue());
         serialPort.setBaudRate(baudRate.getValue());
         serialPort.setNumDataBits(dataBits.getValue());
         serialPort.setNumStopBits(stopBit.getValue());
-        byte[] buffer = new byte[2];
+        serialPort.setParity(paritySetter());
+
+        /**
+         * this thread is used to show real-time broadcast of serial port
+         *
+         */
         thread = new Thread(()->{
             while (loopCondition.getValue()){
+                /**
+                 * platform which set data of chart
+                 */
                 Platform.runLater(()->{
+                    /**
+                     * this loop always will keep size of charts X-axis fixed
+                     */
                     while (series.getData().size()
                             >= SIZE_OF_CHART.intValue()){
                         int i = 0;
                         series.getData().remove(i);
                         ++i;
                     }
-                    serialPort.readBytes(buffer, 2);
-                    XYChart.Data data = new XYChart.Data<>(x+"",
-                            buffer[0]+buffer[1]);
-                    Rectangle rectangle = new Rectangle();
-                    rectangle.setVisible(false);
-                    data.setNode(rectangle);
-                    series.getData().add(data);
-                    textAreaChanger(new Integer((String) data.getXValue()),
-                            (Integer)data.getYValue());
+                    /**
+                     * set data of series here -> y will be the data which been read from
+                     * serial port , x will be set automatically in line 272
+                     * use global variables x and y
+                     *
+                     * please use textAreaChanger method to update dialogPane
+                     *
+                     */
                 });
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                x+= 0.5;
+                x+= STEP_OF_CHART.doubleValue();
             }
         });
         thread.start();
