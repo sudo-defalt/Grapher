7765c5f868b6cdc63e3244a2d724d55873ae7696
ADNAN <adnan.ali.yari@gmail.com>
2019-07-28 13:19:34 +0430

    realese modifies

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,8 @@ package sample;
 
 /**
  *
- * this class is controller of UI
- * main operations of GUI will be setup and triggered here
+ * this class is the controller of UI
+ * main operations of GUI will be set up and triggered here
  *
  * @author Adnan
  * @author Anahita
@@ -31,15 +31,18 @@ import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
@@ -56,25 +59,21 @@ public class Controller {
      *
      */
 
-    enum LOGS{ INFO, ERROR, INTRP}
+    enum LOGS {INFO, ERROR, INTRP}
 
 
     private int setupCount = 0;
-    private static SimpleBooleanProperty condition = new SimpleBooleanProperty(false);
     private SimpleIntegerProperty SIZE_OF_CHART = new SimpleIntegerProperty(20);
     private SimpleIntegerProperty STEP_OF_CHART = new SimpleIntegerProperty(1);
     private static Double SCALE_OF_Y_AXIS;
-    private ScheduledExecutorService scheduledExecutorService;
-    private String pdfName;
-    private String pngName;
-    private String txtName;
-
+    private static String pdfName;
+    private static String pngName;
+    private static String txtName;
 
     /**
      *
-     *  outer dataFields of class which is imported from
+     * outer dataFields of class which is imported from
      * sample.FXML
-     *
      *
      */
     @FXML
@@ -113,9 +112,9 @@ public class Controller {
     XYChart.Series<String, Integer> series = new XYChart.Series<>();
 
     @FXML
-    private void setUp(){
-        if (setupCount == 0){
-            x_axisStep.setOnKeyPressed((keyEvent)->{
+    private void setUp() {
+        if (setupCount == 0) {
+            x_axisStep.setOnKeyPressed((keyEvent) -> {
                 if (keyEvent.getCode() == KeyCode.ENTER)
                     chartChanger();
             });
@@ -139,50 +138,68 @@ public class Controller {
             refresh();
             y_axis.setUpperBound(SCALE_OF_Y_AXIS);
             ++setupCount;
-            thread = new Thread(()->{
-                while (true){
-                    while (condition.getValue()){
-                        if (series.getData().size() > SIZE_OF_CHART.get()){
-                            series.getData().remove(0, series.
-                                    getData().size()-SIZE_OF_CHART.get());
-                        }
-                        Platform.runLater(()->{
-                            XYChart.Data data = new XYChart.Data<>(x+ "",
-                                    (int)(Math.random()*SCALE_OF_Y_AXIS));
-                            series.getData().add(data);
-                            textAreaChanger(LOGS.INFO, (String)data.getXValue(),
-                                    (int) data.getYValue());
-                            });
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        x += STEP_OF_CHART.getValue();
-                    }
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        textAreaChanger(LOGS.INTRP, "<--------Thread"
-                                +" has interrupted-------->",
-                                Integer.MIN_VALUE);
-                        e.printStackTrace();
-                    }
-                }
-            });
-            thread.start();
         }
     }
 
+    private String nameSetter(String name, String extension){
+        if (new File(name+extension).exists()){
+            int i = 1;
+            while (i<100000){
+                if (!(new File(name+"("+i+")"+extension).exists()))
+                    return (name+"("+i+")");
+                ++i;
+            }
+            return (name+"("+100000+")");
+        }else return name;
+    }
+
+    private void nameConfigure(){
+       if (pdfNameTextField.getText().isEmpty()){
+            pdfName = nameSetter("pdfDocument", ".pdf");
+       }else {
+            pdfName = nameSetter(pdfNameTextField.getText(), ".pdf");
+       }
+
+       if (pngNameTextField.getText().isEmpty()){
+            pngName = nameSetter("pngDocument", ".png");
+       }else {
+            pngName = nameSetter(pngNameTextField.getText(), ".png");
+       }
+
+       if (txtNameTextField.getText().isEmpty()){
+            txtName = nameSetter("txtDocument", ".txt");
+       }else {
+            txtName = nameSetter(txtNameTextField.getText(), ".txt");
+       }
+    }
+
+    /**
+     * to get the image and the pdf of our chart
+     */
     @FXML
-    private void export(){
+    private void export() {
+        nameConfigure();
         WritableImage nodeshot = chart.snapshot(new SnapshotParameters(), null);
-        File file = new File("chart.png");
+        File file = new File(pngName+".png");
+        File txtFile = new File(txtName+".txt");
+        try {
+            FileOutputStream writeToFile = new FileOutputStream(txtFile);
+            DataOutputStream fileOutPut = new DataOutputStream(writeToFile);
+            try {
+                fileOutPut.writeUTF(dialogPane.getText());
+            } catch (IOException e) {
+                textAreaChanger(LOGS.INTRP, "Could not write to txt file", Integer.MIN_VALUE);
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            textAreaChanger(LOGS.INTRP, "Could not open text file", Integer.MIN_VALUE);
+            e.printStackTrace();
+        }
         try {
             ImageIO.write(SwingFXUtils.fromFXImage(nodeshot, null), "png", file);
         } catch (IOException e) {
             textAreaChanger(LOGS.INTRP, "<----------Screenshot has not "
-                    +"saved because of interruption------------->",
+                            + "saved because of interruption------------->",
                     Integer.MIN_VALUE);
             System.err.println(e);
         }
@@ -191,23 +208,22 @@ public class Controller {
         PDImageXObject pdimage;
         PDPageContentStream content;
         try {
-            pdimage = PDImageXObject.createFromFile("chart.png",doc);
+            pdimage = PDImageXObject.createFromFile(pngName+".png",doc);
             content = new PDPageContentStream(doc, page);
             content.drawImage(pdimage, 0, 100);
             content.close();
             doc.addPage(page);
-            doc.save("Record.pdf");
+            doc.save(pdfName+".pdf");
             doc.close();
         } catch (IOException ex) {
             textAreaChanger(LOGS.ERROR, "<-------------Screenshot has not saved------------->", Integer.MIN_VALUE);
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
 
     @FXML
-    private void torsion(){
+    private void torsion() {
         SCALE_OF_Y_AXIS = 20.0;
         Main.changeToMain();
     }
@@ -217,23 +233,26 @@ public class Controller {
         Main.changeToMain();
     }
 
-    private void textAreaChanger(LOGS log, String x, int y){
-        dialogPane.appendText("\n["+log.name()+"]\t\t\t\t\t "
-                +"x : "+x
-                +"\t\ty : "+y);
+    private void textAreaChanger(LOGS log, String x, int y) {
+        dialogPane.appendText("\n[" + log.name() + "]\t\t\t\t\t "
+                + "x : " + x
+                + "\t\ty : " + y);
     }
 
+    /**
+     * changes the range of x and y axis
+     */
     @FXML
-    private void chartChanger(){
+    private void chartChanger() {
         if (!sizeOfChartTextArea.getText().isEmpty()
-            && !x_axisStep.getText().isEmpty()) {
+                && !x_axisStep.getText().isEmpty()) {
             SIZE_OF_CHART.set(new Integer(sizeOfChartTextArea.getText()));
             STEP_OF_CHART.set(new Integer(x_axisStep.getText()));
-        }else {
+        } else {
             if (sizeOfChartTextArea.getText().isEmpty()){
                 sizeOfChartTextArea.requestFocus();
                 sizeOfChartTextArea.setFocusColor(Color.RED);
-            }else {
+            } else {
                 x_axisStep.requestFocus();
                 x_axisStep.setFocusColor(Color.RED);
             }
@@ -242,8 +261,8 @@ public class Controller {
 
     /**
      *
-     * this method detect available Comm Ports
-     * and refresh port ChoiceBox
+     * detects available Comm Ports
+     * and refreshes port ChoiceBox
      *
      */
     @FXML
@@ -256,55 +275,57 @@ public class Controller {
         }
     }
 
+    private ScheduledExecutorService scheduledExecutorService;
     @FXML
     private void setButton() {
-        SerialPort serialPort = SerialPort.getCommPort(portNames.getValue());
-        if(serialPort.openPort()) {
-            serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0,0);
-            Scanner portScanner = new Scanner(serialPort.getInputStream());
-            XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        try {
+            SerialPort serialPort = SerialPort.getCommPort(portNames.getValue());
+            if(serialPort.openPort()) {
+                serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0,0);
+                Scanner portScanner = new Scanner(serialPort.getInputStream());
+                XYChart.Series<String, Integer> series = new XYChart.Series<>();
 
-            chart.getData().add(series);
+                chart.getData().add(series);
 
-            final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
 
-            // setup a scheduled executor to periodically put data into the chart
-            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+                // setup a scheduled executor to periodically put data into the chart
+                scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
-            // put data onto graph per second
-            scheduledExecutorService.scheduleAtFixedRate(() -> {
-
-                // Update the chart
-                Platform.runLater(() -> {
-                    // get current time
-                    Date now = new Date();
-                    String newData = portScanner.nextLine();
-                    XYChart.Data data = new XYChart.Data<>(simpleDateFormat.format(now) + "", Integer.parseInt(newData));
-                    series.getData().add(data);
-                    textAreaChanger(LOGS.INFO, (String)data.getXValue(), (Integer) data.getYValue());
-                    while (series.getData().size() > SIZE_OF_CHART.get())
-                        series.getData().remove(0);
-                });
-            }, 0, STEP_OF_CHART.get(), TimeUnit.SECONDS);
-        } else {
-            textAreaChanger(LOGS.ERROR, "<--------------Port has not opened-------------->", Integer.MIN_VALUE);
+                // put data onto graph per second
+                scheduledExecutorService.scheduleAtFixedRate(() -> {
+                    if(iteration % 2 != 0) {
+                        // Update the chart
+                        Platform.runLater(() -> {
+                            // get current time
+                            Date now = new Date();
+                            String newData = portScanner.nextLine();
+                            XYChart.Data data = new XYChart.Data<>(simpleDateFormat.format(now) + "", Integer.parseInt(newData));
+                            series.getData().add(data);
+                            textAreaChanger(LOGS.INFO, (String)data.getXValue(), (Integer) data.getYValue());
+                            while (series.getData().size() > SIZE_OF_CHART.get())
+                                series.getData().remove(0);
+                        });
+                    }
+                }, 0, STEP_OF_CHART.get(), TimeUnit.SECONDS);
+            } else {
+                textAreaChanger(LOGS.ERROR, "<--------------Port has not opened-------------->", Integer.MIN_VALUE);
+            }
+        } catch (Exception e) {
+            System.out.println("No ports found!");
         }
     }
 
     /**
-     * method to control main thread
+     * to control main thread
      */
-    private Thread thread;
     private int iteration = 0;
     @FXML
-    private void start_stop(){
-        if (iteration%2 == 0){
-            System.out.println("!!!!");
-            condition.set(true);
-        }else {
-            System.out.println("@@@@");
-            condition.set(false);
-        }
+    private void start_stop() {
+        //if the user wants to start while no port is selected!
+        if(portNames.getValue() == null) {
+            dialogPane.appendText("Please select a port first!");
+        }else
         ++iteration;
     }
 }
